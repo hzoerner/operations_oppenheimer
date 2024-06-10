afce7ed5214b492c9b5b55e969da5cafc3bdbccc
Simon Irmer <simon.irmer@pagnos.com>
2024-06-10 18:26:55 +0200

    feat: remove large cities, add all admin areas to CISF sheet

--- src/python/location_distance_calculation.py
+++ src/python/location_distance_calculation.py
@@ -16,26 +16,11 @@ cost_factor = 75
 nuclear_factor = 100
 cost_factor *= nuclear_factor
 
-# geo coordinates for each interim storage facility
-isf_coords = {"Gorleben": Point(53.033344625111255, 11.341597461918763),
-              "Ahaus": Point(52.075884172859716, 7.056252098203471),
-              "Brokdorf": Point(53.85053568702555, 9.345557443628627),
-              "Brunsbüttel": Point(53.891632640377, 9.20050890913132),
-              "Biblis": Point(49.70830596704328, 8.411540078513102),
-              "Grafenrheinfeld": Point(49.98379398601132, 10.1866276073602),
-              "Grohnde": Point(52.03567047221345, 9.41068601909146),
-              "Grundemmingen": Point(48.51666979391695, 10.403076045930131),
-              "Isar": Point(48.6075745368956, 12.291958334289768),
-              "Lingen": Point(52.47033938642687, 7.321564619110349),
-              "Krümmel": Point(53.410146129098514, 10.410102309356501),
-              "Neckarwestheim": Point(49.0420294948931, 9.173420061294042),
-              "Philippsburg": Point(49.25285028023963, 8.441867563151883),
-              "Unterweser": Point(53.430121612826966, 8.476324972847728),
-              "Lubmin": Point(54.14141668772474, 13.677069821066244)}
-
-
+# read admin area df
 admin_area_df = gpd.read_file(admin_map_path)
 
+# remove cities
+admin_area_df = admin_area_df[admin_area_df.BEZ.isin(['Kreis', 'Landkreis'])]
 # Define the UTM32 and WGS84 projections
 utm32_crs = "EPSG:32632"  # UTM zone 32N
 wgs84_crs = "EPSG:4326"   # WGS 84
@@ -68,6 +53,25 @@ admin_area_df['geometry'] = admin_area_df['geometry'].apply(transform_geometry)
 # calculate centroids of geometries
 admin_area_df["centroids"] = admin_area_df.geometry.apply(lambda x: centroid(x))
 
+### add coordinates of reactors / ISF
+# geo coordinates for each interim storage facility
+isf_coords = {"Gorleben": Point(53.033344625111255, 11.341597461918763),
+              "Ahaus": Point(52.075884172859716, 7.056252098203471),
+              "Brokdorf": Point(53.85053568702555, 9.345557443628627),
+              "Brunsbüttel": Point(53.891632640377, 9.20050890913132),
+              "Biblis": Point(49.70830596704328, 8.411540078513102),
+              "Grafenrheinfeld": Point(49.98379398601132, 10.1866276073602),
+              "Grohnde": Point(52.03567047221345, 9.41068601909146),
+              "Grundemmingen": Point(48.51666979391695, 10.403076045930131),
+              "Isar": Point(48.6075745368956, 12.291958334289768),
+              "Lingen": Point(52.47033938642687, 7.321564619110349),
+              "Krümmel": Point(53.410146129098514, 10.410102309356501),
+              "Neckarwestheim": Point(49.0420294948931, 9.173420061294042),
+              "Philippsburg": Point(49.25285028023963, 8.441867563151883),
+              "Unterweser": Point(53.430121612826966, 8.476324972847728),
+              "Lubmin": Point(54.14141668772474, 13.677069821066244)}
+
+# transform regional data in dict format
 admin_coords = {k:v for k, v in zip(admin_area_df["GEN"], admin_area_df["centroids"])}
 
 combined_coords = isf_coords | admin_coords
@@ -84,19 +88,34 @@ distance_df = pd.DataFrame(rows_list)
 # save distance data to excel file     
 with pd.ExcelWriter(excel_path, mode="a", engine="openpyxl", if_sheet_exists="replace", ) as writer:
     distance_df.to_excel(writer, sheet_name=distance_sheet, float_format="%.2f", index=False)
-#distance_df.head()
 
 
 # calculate distances for final storage transport
-fsf_coords = {"Saldenberg": (48.77298963391,13.35212619416),
+fsf_coords = {"Saldenberg": Point(48.77298963391,13.35212619416),
               "Lubmin_Endlager": isf_coords["Lubmin"],
-              "Messel": (49.93831226, 8.749268672)}
-storage_coords = isf_coords | admin_coords
+              "Messel": Point(49.93831226, 8.749268672)}
+storage_coords = isf_coords | admin_coords | fsf_coords
 fs_costs = []
 for f, f_c in combined_coords.items():
-    fs_costs.append({"from": f, "costs": cost_factor * distance.distance(f_c, fsf_coords[final_storage]).kilometers})
+    f_coords = (f_c.x, f_c.y)
+    for t, t_c in fsf_coords.items():
+        t_coords = (t_c.x, t_c.y)
+        fs_costs.append({"from": f, "costs": cost_factor * distance.distance(f_coords, t_coords).kilometers})
 fsf_df = pd.DataFrame(fs_costs)
+
+# save distance data to excel file     
 with pd.ExcelWriter(excel_path, mode="a", engine="openpyxl", if_sheet_exists="replace", ) as writer:
     fsf_df.to_excel(writer, sheet_name=final_storage_sheet, float_format="%.2f", index=False)
+
+
+# Update CISF sheet to include all administrative areas
+cisf_df = pd.DataFrame()
+cisf_df["name"] = list(admin_coords.keys())
+cisf_df["capacity"] = 1500
+cisf_df["costs"] = 999
+
+with pd.ExcelWriter(excel_path, mode="a", engine="openpyxl", if_sheet_exists="replace", ) as writer:
+    cisf_df.to_excel(writer, sheet_name="CISF", float_format="%.2f", index=False)
+
 
 print("Done.")
